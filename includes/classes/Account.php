434fa001a53b96862081259f2b0d603857cc0049
Panagiotis Papadopoulos <panagiotpapa@gmail.com>
2023-05-15 13:31:25 +0300

    Validation on register page

---
+++ includes/classes/Account.php
@@ -0,0 +1,61 @@
+<?php
+    class Account {
+
+        private $con;
+        private $errorArray = array();
+
+        public function __construct($con) 
+        {
+            $this->con = $con;
+        }
+
+        public function register($fn, $ln, $un, $em, $em2, $pw, $pw2) {
+            $this->validateFirstName($fn);
+            $this->validateFirstName($ln);
+            $this->validateUsername($un);
+            $this->validateEmails($em, $em2);
+        }
+
+        private function validateFirstName($fn) {
+            if (strlen($fn) < 2 || strlen($fn) > 25) {
+                array_push($this->errorArray, Constants::$firstNameCharacters);
+            } 
+        }
+
+        private function validateLastName($ln) {
+            if (strlen($ln) < 2 || strlen($ln) > 25) {
+                array_push($this->errorArray, Constants::$lastNameCharacters);
+            } 
+        }
+
+        private function validateUsername($un) {
+            if (strlen($un) < 2 || strlen($un) > 25) {
+                array_push($this->errorArray, Constants::$usernameCharacters);
+                return;
+            }
+            
+            $query = $this->con->prepare("SELECT * FROM users WHERE username=:un");
+            $query->bindValue(":un", $un);
+
+            $query->execute();
+
+            if($query->rowCount() !=0) {
+                array_push($this->errorArray, Constants::$usernameTaken);
+            }
+        }
+
+        private function validateEmails($em, $em2) {
+            if($em != $em2) {
+                array_push($this->errorArray, Constants::$emailsDontMatch);
+            }
+        }
+
+        public function getError($error) {
+            if(in_array($error, $this->errorArray)) {
+                return $error;
+            }
+        }
+    };
+
+
+?>
--- register.php
+++ register.php
@@ -1,7 +1,11 @@
 <?php
 
-require_once("includes/classes/FormSanitizer.php");
 require_once("includes/config.php");
+require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/Account.php"); 
+require_once("includes/classes/Constants.php");
+
+    $account = new Account($con);
 
     if (isset($_POST["submitButton"])) {
     
@@ -13,6 +17,7 @@ require_once("includes/config.php");
         $password = formSanitizer::sanitizeFormPassword($_POST["password"]);
         $password2 = formSanitizer::sanitizeFormPassword($_POST["password2"]);
 
+        $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
     }
 ?>
 
@@ -37,12 +42,17 @@ require_once("includes/config.php");
 
             <form method="POST">
 
+                <?php echo $account->getError(Constants::$firstNameCharacters); ?>
                 <input type="text" name="firstName" placeholder="First name" required>
 
+                <?php echo $account->getError(Constants::$lastNameCharacters); ?>
                 <input type="text" name="lastName" placeholder="Last name" required>
 
+                <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                <?php echo $account->getError(Constants::$usernameTaken); ?>
                 <input type="text" name="username" placeholder="Username" required>
 
+                <?php echo $account->getError(Constants::$emailsDontMatch); ?>
                 <input type="email" name="email" placeholder="Email" required>
 
                 <input type="email" name="email2" placeholder="Confirm email" required>
